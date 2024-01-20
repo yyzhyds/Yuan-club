535205180a0691cd56e6451e730621e8c80ae06d
随缘而愈 <1908721295@qq.com>
2024-01-20 12:42:44 +0800

    fix：oss文件模块

--- Yuan_club_subject/Yuan_club_domain/target/generated-sources/annotations/com/zhy/subject/domain/convert/RadioSubjectConverterImpl.java
+++ Yuan_club_subject/Yuan_club_domain/target/generated-sources/annotations/com/zhy/subject/domain/convert/RadioSubjectConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-18T13:10:01+0800",
+    date = "2024-01-20T12:30:03+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_211 (Oracle Corporation)"
 )
 public class RadioSubjectConverterImpl implements RadioSubjectConverter {
